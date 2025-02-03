d3ae11aa174f32cca9bd120104e0c964df4e59dc
Ishaan <ishaanagarwal1203@gmail.com>
2025-02-03 23:35:39 +0530

    news and services

--- laiwyer/src/components/landing.tsx
+++ laiwyer/src/components/landing.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Info from "./info";
-import Navbar from "./navbar";
-
+// import Navbar from "./navbar";
+import Services from "./Services";
+import News from "./News";
 const Landing = () => {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Info />
+      <Services/>
+      <News/>
     </>
   );
 };
